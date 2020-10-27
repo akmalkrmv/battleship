dabc92d76415896ea8f83bf3eb66dfc7c488fedf
akmalkrmv <akmalkarimov94@gmail.com>
2020-10-27 22:44:24 +0300

    player creation

--- src/app/models/player.ts
+++ src/app/models/player.ts
@@ -1,9 +1,36 @@
-import { Field, Ship } from './ship';
+import { Field, FieldJSON } from './field';
+import { User } from './user';
 
 export class Player {
-  public id: string;
-  public ships: Ship[] = [];
+  public uid: string;
+  public photoURL?: string;
+  public displayName: string;
   public battlefield: Field[] = [];
+  public ships: FieldJSON = {};
+  public isComputer = false;
 
-  constructor(public displayName: string, public isComputer = false) {}
+  constructor() {}
+
+  public static create(user: User) {
+    if (!user) return this.empty();
+
+    const player = new Player();
+    player.uid = user.uid;
+    player.photoURL = user.photoURL;
+    player.displayName = user.displayName;
+    return player;
+  }
+
+  public static empty(): Player {
+    const player = new Player();
+    player.displayName = 'Waiting..';
+    return player;
+  }
+
+  public static computer(): Player {
+    const player = new Player();
+    player.displayName = 'PC';
+    player.isComputer = true;
+    return player;
+  }
 }
